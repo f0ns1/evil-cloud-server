7df34323c21a0c116b2ef87f0684420ae8dc1afa
f0ns1 <fonso.gonzalezsan@gmail.com>
2020-05-10 12:31:07 +0200

    Evil-browser with spring security initial version... server for project evil browser and others

---
+++ src/main/java/com/evil/cloud/server/evilserver/EvilServerApplication.java
@@ -0,0 +1,13 @@
+package com.evil.cloud.server.evilserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvilServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvilServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evil/cloud/server/evilserver/EvilServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evil.cloud.server.evilserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvilServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
